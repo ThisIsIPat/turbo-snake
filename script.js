8e92daaac7d931c6723a108af7d1dcf1dfc9cc0b
Patrick Hein <mmf.ipat@gmail.com>
2020-07-16 01:23:51 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+let PIXEL_SIZE = 50;
+let GRID_WIDTH = 9;
+let GRID_HEIGHT = 9;
+
+// Speed is actually slowness
+let SNAKE_START_SPEED = 750;
+let SNAKE_END_SPEED = 150;
+
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+canvas.width = GRID_WIDTH * PIXEL_SIZE;
+canvas.height = GRID_HEIGHT * PIXEL_SIZE;
+
+var oldDirection = 'E';
+var direction = 'E';
+let startX = Math.floor(GRID_WIDTH / 2);
+let startY = Math.floor(GRID_HEIGHT / 2);
+let snake = [[startX + 1, startY],
+            [startX, startY],
+            [startX - 1, startY]];
+console.log(snake);
+var apple = [startX, Math.max(startY - 2, 0)];
+
+document.onkeydown = e => {
+    switch(event.which) {
+        case 38:
+            if (oldDirection != 'S') direction = 'N'
+            break;
+        case 39:
+            if (oldDirection != 'W') direction = 'E'
+            break;
+        case 40:
+            if (oldDirection != 'N') direction = 'S'
+            break;
+        case 37:
+            if (oldDirection != 'E') direction = 'W'
+            break;
+    }
+}
+
+function tick() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    draw(apple[0], apple[1], "red");
+
+    for (let snakeBodyIndex = 0; snakeBodyIndex < snake.length; snakeBodyIndex++) {
+        if (snakeBodyIndex == 0) {
+            let oldSnakeX = snake[0][0];
+            let oldSnakeY = snake[0][1];
+            switch(direction) {
+                case 'N':
+                    if (oldSnakeY <= 0) {
+                        snake.unshift([oldSnakeX, GRID_HEIGHT - 1]);
+                    } else {
+                        snake.unshift([oldSnakeX, oldSnakeY - 1]);
+                    }
+                    break;
+                case 'W':
+                    if (oldSnakeX <= 0) {
+                        snake.unshift([GRID_WIDTH - 1, oldSnakeY]);
+                    } else {
+                        snake.unshift([oldSnakeX - 1, oldSnakeY]);
+                    }
+                    break;
+                case 'S':
+                    if (oldSnakeY >= GRID_HEIGHT - 1) {
+                        snake.unshift([oldSnakeX, 0]);
+                    } else {
+                        snake.unshift([oldSnakeX, oldSnakeY + 1]);
+                    }
+                    break;
+                case 'E':
+                    if (oldSnakeX >= GRID_WIDTH - 1) {
+                        snake.unshift([0, oldSnakeY]);
+                    } else {
+                        snake.unshift([oldSnakeX + 1, oldSnakeY]);
+                    }
+                    break;
+            }
+            oldDirection = direction;
+            if (snake[0][0] != apple[0] || snake[0][1] != apple[1]) {
+                snake.pop();
+            } else {
+                if (snake.length == GRID_WIDTH * GRID_HEIGHT) {
+                    alert('POGCHAMP WTF YOU WON!!!!!');
+
+                    clearInterval(intervalId);
+                    return;
+                }
+
+                clearInterval(intervalId);
+                intervalId = setInterval(tick, SNAKE_END_SPEED + (SNAKE_START_SPEED - SNAKE_END_SPEED) * (1 - snake.length / (GRID_WIDTH * GRID_HEIGHT)) ** 3);
+
+                appleLoop:
+                while (true) {
+                    for (let snek = 0; snek < snake.length; snek++) {
+                        if (snake[snek][0] == apple[0] && snake[snek][1] == apple[1]) {
+                            apple = [Math.floor(Math.random() * GRID_WIDTH), Math.floor(Math.random() * GRID_HEIGHT)];
+                            continue appleLoop;
+                        }
+                    }
+                    break;
+                }
+
+                draw(apple[0], apple[1], "red");
+            }
+            var snakeHead = snake[0]
+            for (let snek = 1; snek < snake.length; snek++) {
+                if (snake[snek][0] == snakeHead[0] && snake[snek][1] == snakeHead[1]) {
+                    alert('LOOOOL YOU LOST');
+
+                    clearInterval(intervalId);
+                    return;
+                }
+            }
+        }
+        
+        let snakeX = snake[snakeBodyIndex][0];
+        let snakeY = snake[snakeBodyIndex][1];
+
+        draw(snakeX, snakeY, "black");
+    }
+}
+
+function draw(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE - 1, PIXEL_SIZE - 1);
+}
+
+var intervalId = setInterval(tick, 700);
+tick(); // To start 0ms tick
